50caec3ebcb7b4f86b03a32f712967594e6819b8
Juan Cardona <jcardonabageth@gmail.com>
2024-05-28 09:45:27 -0500

    refactorizando la logica de crear una nueva tarea en base al id del usuario logeafo

--- src/main/java/com/example/GestorDeTareas/Servicio/TareaServicio.java
+++ src/main/java/com/example/GestorDeTareas/Servicio/TareaServicio.java
@@ -1,13 +1,14 @@
 package com.example.GestorDeTareas.Servicio;
 
 import com.example.GestorDeTareas.DTO.TareasDTO;
+import com.example.GestorDeTareas.Modelos.Tareas;
 
 
 import java.util.List;
 
 public interface TareaServicio {
 
-    public TareasDTO crearTarea(long categoriaId, TareasDTO tareaDto, Long usuarioId);
+    public TareasDTO crearTarea(TareasDTO tareaDto, Long usuarioId);
 
     public List<TareasDTO> obtenerTodasLasTareas();
 
